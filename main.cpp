7a932b2d8502716121080a63356b81c9bda7ed5a
restelli436 <117980583+restelli436@users.noreply.github.com>
2022-11-18 12:29:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main() {
-   return 0;
+
+    float a, b, z;
+
+    int c;
+
+    cin >> a >> b >> c;
+    if (c == 0) {
+        z = (a * b) / 2;
+        cout << z << endl;
+
+
+    }else if (c == 1) {
+        z = (a * a);
+        cout << z << endl;
+
+
+    }else if (c == 2) {
+        z = (a * b);
+        cout << z << endl;
+
+    }else if (c>2) {
+        cout << "operazione non valida" << endl;
+
+
+        return 0;
+    }
 }
